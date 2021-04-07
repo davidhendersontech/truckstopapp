52a3e002af3f0eff36f2400333e022666feec060
notDavidHenderson <sn2catt@gmail.com>
2021-04-07 12:00:57 -0600

    forms are crazy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    location: "",
+    parkingSpots: 0,
+    truckStops: []
+  }
+
+  handleChange = (event) => {
+      this.setState({
+       [event.target.name] : event.target.value 
+
+      })
+      console.log(event.target.name)
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const newTruckStop = {
+      location: this.state.location,
+      parkingSpots: this.state.parkingSpots
+    }
+    this.setState({
+      truckStops: [...this.state.truckStops, {newTruckStop}],
+      // location: "",
+      // parkingSpots: 0
+    })
+  }
+
+  showTruckStops = () => {
+    return this.state.truckStops.map(truckStop => {
+      return <li>
+        <p>Location : {truckStop.location}</p>
+        <p>Parking Spots : {truckStop.parkingSpots}</p>
+      </li>
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Truck Stop App</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label >Location</label>
+          <input 
+            type="text" 
+            name="location" 
+            value={this.state.location}
+            onChange={this.handleChange}
+          />
+          <label >Number of Parking Spots</label>
+          <input 
+            type='number'
+            name='parkingSpots'
+            value={this.state.parkingSpots}  
+            onChange={this.handleChange}   
+          />
+          <button type='submit'>Submit </button>
+        </form>
+
+      <ul>
+        {this.showTruckStops()}
+      </ul>
+
+      </div>
+    )
+  }
 }
+
 
 export default App;
